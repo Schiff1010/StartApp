3d03568b94d30f5d901ee19eefa564b051f37738
misna <schifferulqui4@gmail.com>
2020-07-10 22:45:13 +0700

    latest update

---
+++ App.js
@@ -0,0 +1,27 @@
+/**
+ * Sample React Native App
+ * https://github.com/facebook/react-native
+ *
+ * @format
+ * @flow strict-local
+ */
+
+import React from 'react';
+import {View} from 'react-native';
+import {Container, Content, Text, Button} from 'native-base';
+
+function App() {
+  return (
+    <Container>
+      <Content
+        contentContainerStyle={{
+          justifyContent: 'center',
+          alignItems: 'center',
+        }}>
+        <Text>First App. Edit Here to Custom.</Text>
+      </Content>
+    </Container>
+  );
+}
+
+export default App;
